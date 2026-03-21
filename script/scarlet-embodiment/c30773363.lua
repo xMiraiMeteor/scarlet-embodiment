e7c4a9616832d1b35851f11a153e5c78ed8d6598
Mirai Meteor <xmiraimeteor@gmail.com>
2026-03-21 17:09:11 +0800

    Edited Sakuya the Scarlet Maid's script
    Corrected e2:SetCategory, from (CATEGORY_TOGRAVE) to (CATEGORY_TOHAND+CATEGORY_SEARCH).

--- script/scarlet-embodiment/c30773363.lua
+++ script/scarlet-embodiment/c30773363.lua
@@ -15,7 +15,7 @@ function s.initial_effect(c)
     --Add 1 "Scarlet" monster from Deck to hand
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetCategory(CATEGORY_TOGRAVE)
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY+SEARCH)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1,id)
