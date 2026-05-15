c7a33dfd00046a699e9abc858af95861eb237c3f
Mirai Meteor <xmiraimeteor@gmail.com>
2026-05-15 19:36:22 +0800

    Update c30773363.lua

--- script/c30773363.lua
+++ script/c30773363.lua
@@ -45,20 +45,22 @@ end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
     if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-    local tc=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_HAND|LOCATION_DECK,0,1,1,nil,e,tp):GetFirst()
-    if tc and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE)~0 then
-        tc:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD,0,1)
-        local e1=Effect.CreateEffect(e:GetHandler())
-        e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-        e1:SetCode(EVENT_PHASE+PHASE_END)
-        e1:SetCountLimit(1)
-        e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-        e1:SetLabel(Duel.GetTurnCount()+1)
-        e1:SetLabelObject(tc)
-        e1:SetCondition(s.descon)
-        e1:SetOperation(s.desop)
-        Duel.RegisterEffect(e1,tp)
+    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,1,nil,e,tp):GetFirst()
+    if #g>0 then 
+        Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
     end
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,2))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(s.spelimit)
+	e1:SetReset(RESET_PHASE|PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.spelimit(e,c) 
+    return not c:IsAttribute(ATTRIBUTE_DARK) 
 end
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
     local tc=e:GetLabelObject()
