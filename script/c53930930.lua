58b719bbcd73ab18d1657d1100311b708b9290dc
Mirai Meteor <xmiraimeteor@gmail.com>
2026-06-02 19:31:30 +0800

    Update c53930930.lua

--- script/c53930930.lua
+++ script/c53930930.lua
@@ -71,7 +71,7 @@ end
 function s.rmop(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local g=Duel.GetMatchingGroup(nil,tp,LOCATION_MZONE,0,nil)
-	Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+	Duel.Destroy(g,REASON_EFFECT,LOCATION_REMOVED)
 end
 function s.spfilter(c,e,tp)
 	return c:IsCode(11091144,89155913) and c:IsFaceup() and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
