66044e30a19f60bbef25ea680451cac7ef308648
Mirai Meteor <xmiraimeteor@gmail.com>
2026-06-28 23:36:52 +0800

    Updated the effect of "Scarlet Devil Mansion".

--- script/c74647294.lua
+++ script/c74647294.lua
@@ -5,70 +5,76 @@ function s.initial_effect(c)
     --Activate
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_SET)
+    e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_FREE_CHAIN)
     e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
-    e1:SetOperation(s.setop)
+    e1:SetOperation(s.thop)
     c:RegisterEffect(e1)
     --ATK/DEF up
     local e2=Effect.CreateEffect(c)
     e2:SetType(EFFECT_TYPE_FIELD)
-    e2:SetCode(EFFECT_UPDATE_ATTACK)
+    e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e2:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
     e2:SetRange(LOCATION_FZONE)
-    e2:SetTargetRange(LOCATION_MZONE,0)
-    e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x322))
-    e2:SetCondition(s.atkdefcon)
-    e2:SetValue(600)
+    e2:SetTargetRange(LOCATION_SZONE,0)
+    e2:SetCondition(s.indcon)
+    e2:SetTarget(s.indtg)
+    e2:SetValue(1)
     c:RegisterEffect(e2)
-    local e3=e2:Clone()
-    e3:SetCode(EFFECT_UPDATE_DEFENSE)
-    c:RegisterEffect(e3)
     --Special Summon
-    local e4=Effect.CreateEffect(c)
-    e4:SetDescription(aux.Stringid(id,1))
-    e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e4:SetProperty(EFFECT_FLAG_DELAY|EFFECT_FLAG_CARD_TARGET)
-    e4:SetCode(EVENT_DESTROYED)
-    e4:SetCountLimit(1,id)
-    e4:SetCondition(s.spcon)
-    e4:SetTarget(s.sptg)
-    e4:SetOperation(s.spop)
-    c:RegisterEffect(e4)
-    Duel.AddCustomActivityCounter(id,ACTIVITY_CHAIN,s.chainfilter)
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_LEAVE_FIELD)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetCountLimit(1)
+	e3:SetCondition(s.spcon)
+	e3:SetTarget(s.sptg)
+	e3:SetOperation(s.spop)
+	c:RegisterEffect(e3)
 end
 s.listed_series={0x322}
---Activity counter
-function s.chainfilter(re,tp,cid)
-    return not re:IsActiveType(TYPE_SPELL+TYPE_TRAP)
-end
 --e1 effect code
-function s.setfilter(c)
+function s.thfilter(c)
     --"Scarlet" Quick-Play Spell
-    return c:IsSetCard(0x322) and c:IsQuickPlaySpell() and c:IsSSetable()
+    return c:IsSetCard(0x322) and c:IsQuickPlaySpell() and c:IsAbleToHand() and (c:IsLocation(LOCATION_DECK) or c:IsFaceup())
 end
-function s.setop(e,tp,eg,ep,ev,re,r,rp)
-    local g=Duel.GetMatchingGroup(s.setfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,nil,e,tp)
-    if #g>0 and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 
-        and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
-        local sg=g:Select(tp,1,1,nil)
-        Duel.SSet(tp,sg)
-    end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK|LOCATION_REMOVED,0,1,nil)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK|LOCATION_REMOVED,0,1,1,nil)
+		if #g>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		end
+	end
 end
---e2/e3 effect code
-function s.atkdefcon(e,tp,eg,ep,ev,re,r,rp)
-    return Duel.GetCustomActivityCount(id,1-e:GetHandlerPlayer(),ACTIVITY_CHAIN)>0
+--e2 effect code
+function s.indfilter(c)
+    --Level 7+ DARK Zombie monster
+    return c:IsFaceup() and c:IsLevelAbove(7) and c:IsAttribute(ATTRIBUTE_DARK) and c:IsRace(RACE_ZOMBIE)
 end
---e4 effect code
+function s.indcon(e)
+    return Duel.IsExistingMatchingCard(s.indfilter,e:GetHandler():GetControler(),LOCATION_MZONE,0,1,nil)
+end
+function s.indtg(e,c)
+    return c:IsSpellTrap() and c:IsFacedown()
+end
+--e3 effect code
+function s.spcfilter(c,tp,rp)
+    --DARK monster
+	return c:IsPreviousPosition(POS_FACEUP) and c:IsPreviousControler(tp) and c:GetPreviousAttributeOnField()&ATTRIBUTE_DARK~=0
+		and (c:IsReason(REASON_BATTLE) or (rp~=tp and c:IsReason(REASON_EFFECT)))
+end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    return c:IsPreviousLocation(LOCATION_FZONE) and c:IsPreviousPosition(POS_FACEUP)
+	return eg:IsExists(s.spcfilter,1,nil,tp,rp)
 end
 function s.spfilter(c,e,tp)
-    --DARK monster
-    return c:IsMonster() and c:IsAttribute(ATTRIBUTE_DARK) and c:IsFaceup() and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
+    --"Scarlet" monster
+    return c:IsMonster() and c:IsSetCard(0x322) and c:IsFaceup() and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
     if chkc then return chkc:IsLocation(LOCATION_GRAVE|LOCATION_REMOVED) and chkc:IsControler(tp) and s.spfilter(chkc,e,tp) end
