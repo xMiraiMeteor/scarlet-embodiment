525e0b0a6a4ec59c65976620d19857dec6760312
Mirai Meteor <xmiraimeteor@gmail.com>
2026-09-25 23:39:09 +0800

    Changed value.

--- script/c19099299.lua
+++ script/c19099299.lua
@@ -47,7 +47,7 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
         e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
         e1:SetCode(EFFECT_UPDATE_ATTACK)
         e1:SetValue(-tc:GetLevel()*900)
-        e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+        e1:SetReset(RESETS_STANDARD_PHASE_END)
         tc:RegisterEffect(e1)
     end
 end
